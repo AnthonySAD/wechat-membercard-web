aa2ee12a1a42dd8e2b8c920967aa648d86b5943a
Anthony <Anthony@pengjisoft.com>
2018-12-16 20:57:46 +0800

    first commit

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,4 @@
-//获取应用实例
+// const Page = require('../../utils/ald-stat.js').Page;
 const app = getApp()
 
 Page({
@@ -17,47 +17,48 @@ Page({
               code: 123456,
             }
           ],
+    myCards:[],
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
+
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
+    let thisPage = this
+    wx.showLoading()
+    if (!wx.getStorageSync("token")){
+      console.log('弄token')
+      wx.login({
+        success(res) {
+          if (res.code) {
+            wx.request({
+              method: 'post',
+              url: app.globalData.apiUrl + 'login',
+              data: {
+                code: res.code
+              },
+              success: res => {
+                wx.setStorageSync("token", res.data.data.auth_token)
+                thisPage.getMyCards()
+                wx.hideLoading()
+              }
+            })
+          }
         }
       })
+    }else{
+      this.getMyCards()
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+  getMyCards(){
+    wx.request({
+      url: app.globalData.apiUrl + 'card',
+      header:app.globalData.header,
+      success:res=>{
+        if(!app.checkResult(res)){
+          return false
+        }
+        console.log(res.data.data)
+        wx.setdata
+        wx.hideLoading()
+      }
     })
   }
 })
